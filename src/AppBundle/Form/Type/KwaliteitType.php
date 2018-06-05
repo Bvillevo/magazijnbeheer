3898e61c05a7c53d2cc9ccb7d56b7e11abd7d522
YousefAvans <yzekhnin@avans.nl>
2018-06-05 15:38:46 +0200

    Alles qua opmaak tot nu toe

---
+++ src/AppBundle/Form/Type/KwaliteitType.php
@@ -0,0 +1,46 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+//vul aan als je andere invoerveld-typen wilt gebruiken in je formulier
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+//EntiteitType vervangen door b.v. KlantType
+class KwaliteitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('kwaliteit', ChoiceType::class, array(
+    'choices'  => array(
+        'Nieuw' => 'Nieuw',
+        'Zo goed als nieuw' => 'Zo goed als nieuw',
+        'Gebruikt' => 'Gebruikt',
+        'Schade' => 'Schade',
+        'Onbruikbaar' => 'Onbruikbaar',
+    ),
+));
+
+
+
+
+  }
+
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+			'data_class' => 'AppBundle\Entity\Bestelregel', //Entiteit vervangen door b.v. Klant
+		));
+	}
+
+}
+
+?>
